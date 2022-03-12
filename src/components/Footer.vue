330428b465dc7af664a87bca94aecd89f165445c
Dany Prasetya Angtoni <danyprasetya8@gmail.com>
2022-03-12 12:16:19 +0700

    Mweb Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,36 +1,50 @@
 <template>
-  <div class="bg-light-black text-white px-24 py-16">
-    <section class="text-3xl">
-      <a href="https://instagram.com/futusut?utm_medium=copy_link" target="_blank" class="mr-12 hover:text-gray-300 transition duration-150 ease">
+  <div class="bg-light-black text-white px-10 py-8 xl:px-24 xl:py-16">
+    <section class="text-xl xl:text-3xl flex flex-col xl:flex-row">
+      <a
+        href="https://instagram.com/futusut?utm_medium=copy_link"
+        target="_blank"
+        class="mb-4 mr-12 hover:text-gray-300 transition duration-150 ease"
+      >
         Instagram
       </a>
-      <a href="https://api.whatsapp.com/send?phone=6281992123123" target="_blank" class="mr-12 hover:text-gray-300 transition duration-150 ease">
+      <a
+        href="https://api.whatsapp.com/send?phone=6281992123123"
+        target="_blank"
+        class="mb-4 mr-12 hover:text-gray-300 transition duration-150 ease"
+      >
         Whatsapp
       </a>
-      <a href="" target="_blank" class="hover:text-gray-300 transition duration-150 ease">
+      <a
+        href=""
+        target="_blank"
+        class="hover:text-gray-300 transition duration-150 ease"
+      >
         Email
       </a>
     </section>
 
-    <section class="mt-16 mb-14 flex">
-      <div class="text-black p-3 bg-white w-1/3">
+    <section class="mt-12 xl:mt-16 mb-8 xl:mb-14 flex flex-col xl:flex-row">
+      <div class="text-black p-2 xl:p-3 bg-white xl:w-1/3 mb-4 xl:mb-0">
         <input
           type="text"
           class="outline-none w-full"
           placeholder="Enter your email here"
         >
       </div>
-      <button class="ml-8 text-lg hover:text-gray-300 transition duration-150 ease">
+      <button class="xl:ml-8 text-lg hover:text-gray-300 transition duration-150 ease">
         Subscribe Now
       </button>
     </section>
 
-    <section class="text-gray-400 w-1/2 flex justify-between">
-      &copy; 2022 by Futusut
+    <section class="text-gray-400 xl:w-1/2 flex flex-col items-center xl:flex-row justify-between">
+      <div class="mb-3 xl:mb-0">
+        &copy; 2022 by Futusut
+      </div>
 
-      <div>
-        <RouterLink :to="faq" class="mr-8 underline">FAQ</RouterLink>
-        <RouterLink :to="reschedulePolicy" class="mr-8 underline">Reschedule Policy</RouterLink>
+      <div class="flex flex-col items-center xl:flex-row">
+        <RouterLink :to="faq" class="xl:mr-8 underline">FAQ</RouterLink>
+        <RouterLink :to="reschedulePolicy" class="xl:mr-8 underline">Reschedule Policy</RouterLink>
         <RouterLink :to="studioPolicy" class="underline">Studio Policy & Protocols</RouterLink>
       </div>
     </section>
